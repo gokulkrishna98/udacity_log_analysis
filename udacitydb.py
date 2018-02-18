a770930d500a4e521cf4d31b71c108921ba8c48b
gokulkrishna98 <gokulkrishna98@gmail.com>
2018-02-18 11:09:43 +0530

    Update udacitydb.py
    update 1

--- udacitydb.py
+++ udacitydb.py
@@ -2,10 +2,9 @@
 import psycopg2
 import operator
 
-# function to print the top 3 articles !!!
-
 
 def populararticles():
+    """function to print the top 3 articles !!!"""
     # connecting to database
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
@@ -16,33 +15,21 @@ def populararticles():
     making sure the view was successfull and ordering it in
     descending order;
     '''
-    c.execute("""select split_part(path,'/',3), count(*)
-                as count from log where status like '200 OK'
-                group by path
-                order by count desc;""")
+    c.execute("""select articles.title,articlecount.count from articlecount
+                inner join articles on upper(articles.title)
+                like concat('%',upper(pathtitle),'%') limit 3;""")
     name = c.fetchall()
 
-    # replacing - witn <space> and checking it in titles article
-    # to get the title name of articles
-    for i in range(1, 4):
-
-        articlepath = name[i][0].replace("-", " ")
-        end = len(articlepath)
-        s = """select title from articles where
-            title like '%""" + articlepath[1:end] + "%'"
-        c.execute(s)
-        new = c.fetchall()
-
-        # printing title and its view count
-        print('%s - %d' % (new[0][0], name[i][1]))
+    for i in name:
+        printstring = "%s - %ld" % (i[0], i[1])
+        print(printstring)
 
     db.close()
     return
 
 
-# function to order authors by popularity
 def popularauthors():
-
+    """function to order authors by popularity"""
     # connencting to database .....
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
@@ -82,7 +69,6 @@ def popularauthors():
                 """ + """ like upper('%""" + articlepath + "%')"
             c.execute(s)
             authorid = c.fetchall()
-
             # counting views for author and if error pass it
             try:
                 articlesvotes[authorid[0][0]] += i[1]
@@ -121,49 +107,47 @@ def popularauthors():
     return
 
 
-# function to display date with more than 1.1% error
 def errorcount():
-
+    """function to display date with more than 1.1% error"""
     # connecting to database
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     # selecting date from time and counting article views with no error and
     # grouping and ordering by dates
 
-    c.execute("""select to_char(time::timestamp::date,'Mon dd,yyyy')
-                as dates,count(*)
-                as count from log where status='200 OK' group by dates order
+    c.execute("""select to_char(time::timestamp::date,'Mon dd,yyyy') as dates,
+                count((select status where status = '200 OK')) as success,
+                count((select status where status != '200 OK')) as error
+                from log group by dates order
                 by dates;""")
     # getting it to an object
-    successviewdata = c.fetchall()
-
-    c.execute("""select time::timestamp::date as dates, count(*)
-                as count from log where status !='200 OK' group by dates order
-                by dates;""")
-    errorviewdata = c.fetchall()
-
+    viewdata = c.fetchall()
     # selecting date from time and counting article views with error and
     # grouping and ordering by dates
-
-    count = 0
-    for i in successviewdata:
+    for i in viewdata:
         # calculating error percentage for each date
-        errorpercent = (float(errorviewdata[count][1]) / float(i[1])) * 100
+        a = float(i[2])
+        b = float(i[1])
+        errorpercent = (a / (a + b)) * 100
 
         # displaying date with more than 1.1% error percentage
         if errorpercent > 1.1:
             print("%s - %f" % (i[0], errorpercent))
 
-        count = count+1
 
-# calling respective functions
-print("****Top 3 popular articles****")
-print("------------------------------\n")
-populararticles()
-print("\n****Popular authors & their viewcount****")
-print("-------------------------------------------\n")
-popularauthors()
-print("\n")
-print("\n****Days with more than 1 percent error****")
-print("wait.......\n")
-errorcount()
+def main():
+    # calling respective functions
+    print("****Top 3 popular articles****")
+    print("------------------------------\n")
+    populararticles()
+    print("\n****Popular authors & their viewcount****")
+    print("-------------------------------------------\n")
+    popularauthors()
+    print("\n")
+    print("\n****Days with more than 1 percent error****")
+    print("wait.......\n")
+    errorcount()
+
+
+if __name__ == "__main__":
+    main()
